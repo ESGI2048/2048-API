9f6a6466cf49e6b374958d773e91b21b44afcfee
LAURENT Louis <louis_laurent@aol.fr>
2019-10-16 08:19:51 +0000

    feat(env): add env system with dotenv

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const bodyParser = require('express');
 const morgan = require('morgan'); 
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.listen(port, () => {console.log(`The app listening on ${port} ...`)});
 
