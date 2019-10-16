a04817a3f6292837451f3a4e93e30bb02bc7d3ce
Thomas BRAZE <thomas.braze@opalesystems.com>
2019-10-16 13:45:40 +0200

    database built and postgres linked with nodeJS

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const bodyParser = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan'); 
 const dotenv = require('dotenv');
+const model = require('./models');
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8888;
 
+model.sequelize.authenticate()
+    .then(() => {
+        console.log('Connected !') ;
+        return model.sequelize.sync({ force : false}); // force : if true will drop the table first and re-create it afterwards
+
+    })
+    .catch( (err) => {
+        console.log('Not connected, something went wrong \n' +  err) ;
+    });
+
+const port = process.env.PORT || 8888;
 app.listen(port, () => {console.log(`The app listening on ${port} ...`)});
 
--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 'use strict' ;
 
 
+
+
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User',{
         firstName : {type : DataTypes.STRING, allowNull: false},
@@ -8,7 +10,27 @@ module.exports = (sequelize, DataTypes) => {
         login:  {type : DataTypes.STRING, unique : true, allowNull: false},
         password:  {type : DataTypes.STRING, allowNull: false},
         email : {type : DataTypes.STRING, unique: true, allowNull: false, validate :{isEmail : true}},
-        score : {type : DataTypes.INTEGER, allowNull: true}
+        score : {type : DataTypes.INTEGER, allowNull: true},
+        phone: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: false,
+            unique: true,
+            validate: {
+                isValidPhoneNo: function(value) {
+                    if (!value) return value;
+
+                    const regexp = /^[0-9]+$/;
+                    let values = (Array.isArray(value)) ? value : [value];
+
+                    values.forEach(function(val) {
+                        if (!regexp.test(val)) {
+                            throw new Error("Number only is allowed.");
+                        }
+                    });
+                    return value;
+                }
+            }
+        }
     },{
         tableName : 'User',
         paranoid : true,
