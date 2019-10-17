82a229dfcf9ce25fa19f77a567dbcece031569c6
SystemGlitch <jeremy.la@outlook.fr>
2019-10-17 10:58:18 +0200

    Add admin auth route

--- routes/AuthenticationRouter.js
+++ routes/AuthenticationRouter.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const controllers = require('../controllers');
+const authMiddleware = require('../middlewares/AdminAuth.js');
 const AuthenticationController = controllers.AuthenticationController;
 
 class AuthenticationRouter{
 
     setRoutes(router){
+        router.get('/admin', authMiddleware.verifyBasicAuth, (req, res, next) => {
+            res.sendStatus(204);
+        });
+
         router.get('/:login', async (req, res) => {
             try {
                 const result = await AuthenticationController.getUserByLogin(req.params.login);
